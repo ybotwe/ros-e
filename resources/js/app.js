ee5100dfb623287fa5d8ccff2f74aaa7a49109f0
OlukoyaDaniel <danolukoya19@gmail.com>
2020-02-04 03:39:20 +0000

    Vue Page routing

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,20 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+    { path: '/dashboard', component: require('./components/Dashboard.vue').default },
+    { path: '/profile', component: require('./components/Profile.vue').default }
+]
+
+const router = new VueRouter({
+    mode:'history',
+    routes // short for `routes: routes`
+})
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -29,4 +43,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    router
 });
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('{path}', 'HomeController@index')->where('path', '([A-z\d\-\/_.]+)');
