a351ad3d55c6469e35600bebbd24e7e95204f106
OlukoyaDaniel <danolukoya19@gmail.com>
2020-02-04 08:32:30 +0000

    VForm & First components created

---
+++ resources/js/components/Users.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="container">
+        
+          <div class="col-md-12">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">All Users</h3>
+
+                <div class="card-tools">
+                  <button class="btn btn-success" data-toggle="modal" data-target="#newUser"><i class="fas fa-plus"></i> New User</button>
+                  <!-- Modal -->
+                <div class="modal fade" id="newUser" tabindex="-1" role="dialog" aria-labelledby="newUserTitle" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="newUserTitle">Add new user</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group">
+                        <input v-model="form.username" placeholder='Full name' type="text" name="username"
+                            class="form-control" :class="{ 'is-invalid': form.errors.has('username') }">
+                        <has-error :form="form" field="username"></has-error>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-success"><i class="fas fa-plus"></i> Add User</button>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                </div>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body table-responsive p-0">
+                <table class="table table-hover">
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Name</th>
+                      <th>Email</th>
+                      <th>Role</th>
+                      <th>Actions</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>183</td>
+                      <td>John Doe</td>
+                      <td>11-7-2014</td>
+                      <td><span class="tag tag-success">Approved</span></td>
+                      <td>
+                          <a href="#"><i class="fas fa-pen"></i></a>
+                          <a href="#"><i class="fas fa-trash red"></i></a>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                form:new Form({
+                    name: '',
+                    email: '',
+                    password: '',
+                    role: '',
+                    bio: '',
+                    photo: '',
+                })
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -79,7 +79,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
               </li>
               
           <li class="nav-item has-treeview">
-            <a href="#" class="nav-link active">
+            <a href="#" class="nav-link">
               <i class="nav-icon fas fa-cog"></i>
               <p>
                 Management
@@ -88,17 +88,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="#" class="nav-link active">
-                  <i class="fas fa-times-circle"></i>
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Active Page</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Inactive Page</p>
-                </a>
+                <router-link to="/users" class="nav-link">
+                  <i class="fas fa-users nav-icon"></i>
+                  <p>Users</p>
+                </router-link>
               </li>
             </ul>
           </li>
@@ -111,12 +104,16 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </router-link>
           </li>
           <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-sign-out-alt"></i>
-              <p>
-                Logout
-              </p>
+            <a class="nav-link" href="{{ route('logout') }}"
+                onclick="event.preventDefault();
+                              document.getElementById('logout-form').submit();">
+                              <i class="nav-icon fas fa-sign-out-alt"></i>
+                              <p>{{ __('Logout') }}</p>
             </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
           </li>
         </ul>
       </nav>
@@ -130,7 +127,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Main content -->
     <div class="content">
       <div class="container-fluid">
-        <router-view></router-view>
+        <router-view class="mt-3"></router-view>
       </div><!-- /.container-fluid -->
     </div>
     <!-- /.content -->
